061e564be1367958abd2b01920d1ed5e404f569f
jankuca <jan@jankuca.com>
2021-06-01 13:22:38 +0200

    fix OpenDesignApiError typing

--- api/src/open-design-api-error.ts
+++ api/src/open-design-api-error.ts
@@ -1,18 +1,27 @@
-import { error } from 'console'
-import type { OperationResponse } from './types/operation.type'
-
 export class OpenDesignApiError extends Error {
   name = 'OpenDesignApiError'
 
-  constructor(res: OperationResponse<any>, message: string | null = null) {
+  constructor(
+    res: {
+      statusCode: number
+      body?: {
+        'error'?: {
+          'code': string
+          'message'?: string
+          'docs_url'?: string
+        }
+      } & Record<string, unknown>
+    },
+    message: string | null = null
+  ) {
     super()
 
-    const body = 'body' in res ? res.body : {}
+    const body = 'body' in res ? res.body || {} : {}
     const errorInfo: {
       'code': string
       'message'?: string
       'docs_url'?: string
-    } | null = 'error' in body ? body['error'] : null
+    } | null = 'error' in body ? body['error'] || null : null
 
     const apiErrorStr = errorInfo
       ? `${errorInfo['code']}${
